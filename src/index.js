3228551ea09f9fc24d5e53fc281bb7e894d8e6fc
Игорь Процюк < >
2018-06-08 23:13:55 +0300

    created 13 hm

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Model from './Model';
+import View from './View';
+import Controller from './Controller';
+import './styles.css';
+
+const model = new Model();
+const view = new View();
+
+new Controller(model, view);
